0a7cb09270da8be95cfb4a24546b5fa4c6df7ab6
Sujal <web.sujal@gmail.com>
2024-01-11 21:45:33 +0530

    added about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,44 @@
 import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
-      <Image src="/astronaut.png" alt="" height={500} width={500} />
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1 className={styles.title}>
+          We create digital ideas that are bigger, bolder, braver and better.
+        </h1>
+        <p className={styles.desc}>
+          We create digital ideas that are bigger, bolder, braver and better. We
+          believe in good ideas flexibility and precision. We&#39;re world&#39;s
+          Our Special Team best consulting & finance solution provider. Wide
+          range of web and software development services.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 k+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 k+</h1>
+            <p>Year of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 k+</h1>
+            <p>Year of experience</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="/astronaut.png"
+          alt="about image"
+          fill
+        />
+      </div>
     </div>
   );
 };
