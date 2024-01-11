1eb909f50259f80ab73851c11005ed4f9ed91e67
Sujal <web.sujal@gmail.com>
2024-01-11 22:02:42 +0530

    completed contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src="/astronaut.png"
+          alt="form page"
+          fill
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea cols="30" rows="10" placeholder="Message"></textarea>
+          <button>send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
