a0f5f89f8f228b6a78eea9491c9c4a2d95330494
Sujal <web.sujal@gmail.com>
2024-02-13 14:26:33 +0530

    add post model

--- src/lib/models.js
+++ src/lib/models.js
@@ -31,4 +31,31 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.models.User || mongoose.model("User", userSchema);
+export const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
